a8b9b9bf5b06025d62d1311cdae7d4e2c3c48e43
leilanikulkarni <lkulkarni@fwparker.org>
2020-01-28 14:23:56 -0600

    Added import notes

---
+++ Notes/_01_modules.py
@@ -0,0 +1,24 @@
+# MODULES, IMPORTS and LIBRARIES
+import turtle
+from math import pi, cos  # import single items from a lib
+from random import *  # imports everything locally
+import random as r  # use your own keyword
+import import_me
+
+print(pi)
+
+
+if __name__ == "__main__":
+    # this code only runs when you run THIS FILE first
+    print("Hello")
+    # my_turtle = turtle.Turtle()
+    print(cos(0.5))
+    # print(sin(0.5))
+    print(random.randrange(1.7))
+    print(r.random())
+    print(import_me.x)
+
+
+
+
+
---
+++ Notes/import_me.py
@@ -0,0 +1,7 @@
+
+x = 5
+y = 10
+print("x + y =", x + y)
+
+if __name__ == "__main__":
+    print("x + y =", x + y)
